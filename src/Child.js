864a219e10c95da20e0de94aec894e8c5470e794
xwm <944818410@qq.com>
2021-09-12 14:22:46 +0800

    class3

---
+++ src/Child.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './Child.module.css'
+
+// console.log(style)
+export default function Child() {
+    return (
+        <div>
+            <ul>
+                <li className={style.item}>child-11111</li>
+                <li className={style.item}>child-11111</li>
+            </ul>
+        </div>
+    )
+}
