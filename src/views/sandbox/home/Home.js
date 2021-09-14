509d8a755f407ef176dd107244bb79b75cfd9a61
xwm <944818410@qq.com>
2021-09-14 22:22:32 +0800

    JsonServer-1

--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Button } from 'antd';
-
+import axios from "axios"
 export default function Home() {
+    const ajax = ()=>{
+         axios.get("http://localhost:8000/posts").then(res=>{
+             console.log(res.data);
+         })
+    }
     return (
         <div>
-           <Button type="primary">Button</Button>
+           <Button type="primary" onClick={ajax}>Button</Button>
         </div>
     )
 }
