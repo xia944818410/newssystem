a15cb79541b06f7a4ca6801942b17e0f01ddaa7f
xwm <944818410@qq.com>
2021-11-16 15:38:57 +0800

    78+9

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,78 +1,87 @@
-import React, {useEffect,useState} from 'react'
-import { Layout, Menu } from 'antd';
-import './index.css'
-import {withRouter} from 'react-router-dom'
-import { UserOutlined, VideoCameraOutlined, UploadOutlined} from '@ant-design/icons';
-import axios from "axios"
-const { Sider } = Layout
+import React, { useEffect, useState } from "react";
+import { Layout, Menu } from "antd";
+import "./index.css";
+import { withRouter } from "react-router-dom";
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+import axios from "axios";
+const { Sider } = Layout;
 const { SubMenu } = Menu;
 
 const iconList = {
-    "/home":<UserOutlined />,
-    "/user-manage":<UserOutlined />,
-    "/user-manage/list":<UserOutlined />,
-    "/right-manage":<UserOutlined />,
-    "/right-manage/role/list":<UserOutlined />,
-    "/right-manage/right/list":<UserOutlined />
-  }
+  "/home": <UserOutlined />,
+  "/user-manage": <UserOutlined />,
+  "/user-manage/list": <UserOutlined />,
+  "/right-manage": <UserOutlined />,
+  "/right-manage/role/list": <UserOutlined />,
+  "/right-manage/right/list": <UserOutlined />,
+};
 
 function SideMenu(props) {
-    const [menu, setMenu] = useState([])
-    useEffect(()=>{
-        axios.get("http://localhost:5000/rights?_embed=children").then(res=>{
-        //  console.log("sidemenu",res.data);
-        setMenu(res.data)
-       })
-    },[])
-    /*  rights:拿到当前登录用户所具有的权限列表 */
-    // console.log("11",JSON.parse(localStorage.getItem("token")));
-    const {role:{rights}} = JSON.parse(localStorage.getItem("token"))
-    /*  rights.includes(item.key):rights里面的权限必须包含，导航栏item.key，且为真，侧边栏才有机会渲染*/
-    const checkPagePermission = (item)=>{
-        return  item.pagepermisson===1 && rights.includes(item.key)
-    }
+  const [menu, setMenu] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/rights?_embed=children").then((res) => {
+      //  console.log("sidemenu",res.data);
+      setMenu(res.data);
+    });
+  }, []);
+  /*  rights:拿到当前登录用户所具有的权限列表 */
+  // console.log("11",JSON.parse(localStorage.getItem("token")));
+  const {
+    role: { rights },
+  } = JSON.parse(localStorage.getItem("token"));
+  /*  rights.includes(item.key):rights里面的权限必须包含，导航栏item.key，且为真，侧边栏才有机会渲染*/
+  const checkPagePermission = (item) => {
+    return item.pagepermisson === 1 && rights.includes(item.key);
+  };
 
-    const renderMenu = (menuList) =>{
-      return menuList.map(item =>{
-          if(item.children?.length>0 && checkPagePermission(item)){
-            return  <SubMenu
-                      key={item.key}
-                      icon={iconList[item.key]}
-                      title={item.title}
-                    >
-                         {renderMenu(item.children)}
-                    </SubMenu>
-          }
-            return   checkPagePermission(item) &&
-                    <Menu.Item 
-                        key={item.key}
-                        icon={iconList[item.key]}
-                        onClick={()=>{
-                        //   console.log("item.key",props);
-                        props.history.push(item.key)
-                        }}
-                    >
-                        {item.title}
-                    </Menu.Item>
-      })
-    }
-    // console.log("p",props.location.pathname);
-    const selectKeys = [props.location.pathname]
-    // console.log("props.location.pathname",props.location.pathname);
-    const openKey = ["/" + props.location.pathname.split("/")[1]]
-    return (
-        <Sider trigger={null} collapsible collapsed={false}>
-              <div style={{display:"flex",height:"100%","flexDirection":"column"}}>  
-                    <div className="logo" >新闻管理发布系统</div>
-                        {/* defaultSelectedKeys后面的参数表示默认展示的内容*/}
-                    <div style={{flex:1,"overflow":"auto"}}>        
-                            <Menu theme="dark" mode="inline"
-                                  //selectedKeys为当前选中的菜单项 key 数组，也即默认选中的二级菜单项
-                                  selectedKeys={selectKeys}
-                                  //defaultOpenKeys为初始展开的 SubMenu 菜单项 key 数组，也即默认选中的一级菜单项
-                                  defaultOpenKeys={openKey}
-                            >
-                                {/* <Menu.Item key="1" icon={<UserOutlined />}>
+  const renderMenu = (menuList) => {
+    return menuList.map((item) => {
+      if (item.children?.length > 0 && checkPagePermission(item)) {
+        return (
+          <SubMenu key={item.key} icon={iconList[item.key]} title={item.title}>
+            {renderMenu(item.children)}
+          </SubMenu>
+        );
+      }
+      return (
+        checkPagePermission(item) && (
+          <Menu.Item
+            key={item.key}
+            icon={iconList[item.key]}
+            onClick={() => {
+              //   console.log("item.key",props);
+              props.history.push(item.key);
+            }}
+          >
+            {item.title}
+          </Menu.Item>
+        )
+      );
+    });
+  };
+  // console.log("p",props.location.pathname);
+  const selectKeys = [props.location.pathname];
+  // console.log("props.location.pathname",props.location.pathname);
+  const openKey = ["/" + props.location.pathname.split("/")[1]];
+  return (
+    <Sider trigger={null} collapsible collapsed={false}>
+      <div style={{ display: "flex", height: "100%", flexDirection: "column" }}>
+        <div className="logo">新闻管理发布系统</div>
+        {/* defaultSelectedKeys后面的参数表示默认展示的内容*/}
+        <div style={{ flex: 1, overflow: "auto" }}>
+          <Menu
+            theme="dark"
+            mode="inline"
+            //selectedKeys为当前选中的菜单项 key 数组，也即默认选中的二级菜单项
+            selectedKeys={selectKeys}
+            //defaultOpenKeys为初始展开的 SubMenu 菜单项 key 数组，也即默认选中的一级菜单项
+            defaultOpenKeys={openKey}
+          >
+            {/* <Menu.Item key="1" icon={<UserOutlined />}>
                                     首页1
                                 </Menu.Item>
                             <Menu.Item key="2" icon={<UserOutlined />}>
@@ -85,11 +94,11 @@ function SideMenu(props) {
                                         <Menu.Item key="11">Option 12</Menu.Item>
                                         <Menu.Item key="2545">Option 34</Menu.Item>
                                 </SubMenu>  */}
-                                {renderMenu(menu)}
-                            </Menu>
-                    </div>        
-             </div>        
-        </Sider>
-    )
+            {renderMenu(menu)}
+          </Menu>
+        </div>
+      </div>
+    </Sider>
+  );
 }
-export default withRouter(SideMenu)
+export default withRouter(SideMenu);
--- src/components/sandbox/TopHeader.js
+++ src/components/sandbox/TopHeader.js
@@ -1,54 +1,58 @@
-import React,{useState} from 'react'
-import { Layout ,Menu, Dropdown, Avatar } from 'antd';
-import { MenuUnfoldOutlined,MenuFoldOutlined ,UserOutlined} from '@ant-design/icons';
-import {withRouter} from 'react-router-dom'
+import React, { useState } from "react";
+import { Layout, Menu, Dropdown, Avatar } from "antd";
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { withRouter } from "react-router-dom";
 const { Header } = Layout;
 
 function TopHeader(props) {
-    const [collapsed,setCollapsed] = useState(false)
-    const changeCollapsed = ()=>{
-        setCollapsed(!collapsed)
-    }
-    /* localStorage.getItem("token")是表单收集的数据;结构解析取出roleName和username*/
-    const {role:{rights,roleName},username} = JSON.parse(localStorage.getItem("token"))
-    
-    const menu = (
-        <Menu>
-            <Menu.Item>
-                {roleName}
-            </Menu.Item>
-            <Menu.Item 
-               danger
-               onClick={()=>{
-                  localStorage.removeItem("token")
-                  /* 重定向到login组件 */
-                  props.history.replace("/login")
-               }}
-            >
-                退出
-            </Menu.Item>
-        </Menu>
-      );
+  const [collapsed, setCollapsed] = useState(false);
+  const changeCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+  /* localStorage.getItem("token")是表单收集的数据;结构解析取出roleName和username*/
+  const {
+    role: { rights, roleName },
+    username,
+  } = JSON.parse(localStorage.getItem("token"));
 
-    return (
-        <Header className="site-layout-background" style={{ padding: '0 16px' }}>
-            {
-                collapsed ? <MenuUnfoldOutlined onClick={changeCollapsed}/>
-                : <MenuFoldOutlined onClick={changeCollapsed}/>
-            }
-            <div style={{float:"right"}}>
-                <span>
-                    欢迎
-                    <span style={{color:"#1890ff"}}>
-                        {username}
-                    </span>
-                    回来
-                </span>
-                <Dropdown overlay={menu}>
-                    <Avatar size="large" icon={<UserOutlined />} />
-                </Dropdown>
-            </div>
-      </Header>
-    )
+  const menu = (
+    <Menu>
+      <Menu.Item>{roleName}</Menu.Item>
+      <Menu.Item
+        danger
+        onClick={() => {
+          localStorage.removeItem("token");
+          /* 重定向到login组件 */
+          props.history.replace("/login");
+        }}
+      >
+        退出
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Header className="site-layout-background" style={{ padding: "0 16px" }}>
+      {collapsed ? (
+        <MenuUnfoldOutlined onClick={changeCollapsed} />
+      ) : (
+        <MenuFoldOutlined onClick={changeCollapsed} />
+      )}
+      <div style={{ float: "right" }}>
+        <span>
+          欢迎
+          <span style={{ color: "#1890ff" }}>{username}</span>
+          回来
+        </span>
+        <Dropdown overlay={menu}>
+          <Avatar size="large" icon={<UserOutlined />} />
+        </Dropdown>
+      </div>
+    </Header>
+  );
 }
-export default withRouter(TopHeader)
+export default withRouter(TopHeader);
